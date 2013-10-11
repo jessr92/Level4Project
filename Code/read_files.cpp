ccf38b59c81d686248866111bf8e510746de0637
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2013-10-11 18:57:02 +0100

    Minor refactor to move docaddr gen to read_files

--- Code/read_files.cpp
+++ Code/read_files.cpp
@@ -37,3 +37,35 @@ const std::vector<word_t> *readProfileFromFile(std::string memcontent_file)
     bfh.close ();
     return profile;
 }
+
+std::vector<unsigned int> *getDocumentAddresses(const std::vector<word_t> *collection)
+{
+    std::vector<unsigned int> *docAddresses = new std::vector<unsigned int>;
+    // Address represents the word in collection the document id resides in.
+    unsigned int address = 0;
+    unsigned int numberOfDocuments = 0;
+    // Placeholder at beginning of vector until we know true value.
+    docAddresses->push_back(numberOfDocuments);
+    // Loop over the entire collection to find the words representing documents
+    for (std::vector<word_t>::const_iterator iter = collection->begin(); iter != collection->end(); ++iter)
+    {
+        word_t word = *iter;
+        // Documents have their last four bits as zero (no ngrams so no length)
+        if ((word & 0xF) == 0)
+        {
+            // We've found a document
+            ++numberOfDocuments;
+            // Add document address to vector
+            docAddresses->push_back(address);
+        }
+        // Iterate the address after each iteration
+        ++address;
+    }
+    // Store the length of the collection (in bytes) at the end of the vector
+    // so we know where the collection and thus last document ends.
+    docAddresses->push_back(collection->size() * sizeof(word_t)); // NEEDED?
+    // Update the 0th element of the vector that was originally a placeholder
+    // to the total number of documents in the vector
+    docAddresses->at(0) = numberOfDocuments;
+    return docAddresses;
+}
--- Code/read_files.h
+++ Code/read_files.h
@@ -7,3 +7,5 @@ typedef uint64_t word_t;
 const std::vector<word_t> *loadParsedCollectionFromFile(std::string collection_file);
 
 const std::vector<word_t> *readProfileFromFile(std::string memcontent_file);
+
+std::vector<unsigned int> *getDocumentAddresses(const std::vector<word_t> *collection);
