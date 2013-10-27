c544d7cad41d6926f93a1d50f9dd46ac713f5e81
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2013-10-27 16:58:29 +0000

    Adds structure for gpu implementation

---
+++ Code/gpu_implementation_no_wrapper.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "read_files.h"
+#include "shared_details.h"
+
+#define THRESHOLD 0
+
+void executeGPUImplementation(const std::vector<word_t> *collection,
+                              const std::vector<word_t> *profile,
+                              std::vector<unsigned int> *docAddresses,
+                              const std::vector<word_t> *bloomFilter)
+{
+    unsigned long hamCount = 0;
+    unsigned long spamCount = 0;
+    unsigned long *scores = new unsigned long[docAddresses->at(0)];
+    for (word_t i = 0; i < docAddresses->at(0); i++)
+    {
+        scores[i] = 0;
+    }
+    for (word_t i = 0; i < docAddresses->at(0); ++i)
+    {
+        scores[i] > THRESHOLD ? ++spamCount : ++hamCount;
+        std::cout << (collection->at(docAddresses->at(i + 1)) >> 4) << ":" << scores[i] << std::endl;
+    }
+    std::cout << "Ham documents: " << hamCount << std::endl;
+    std::cout << "Spam documents " << spamCount << std::endl;
+    delete [] scores;
+}
+
+int main()
+{
+    const std::vector<word_t> *bloomFilter = loadParsedCollectionFromFile(BLOOM_FILTER_FILE);
+    std::cout << "bloomfilter.raw: " << bloomFilter->size() << std::endl;
+    const std::vector<word_t> *collection = loadParsedCollectionFromFile("collection.raw");
+    std::cout << "collection.raw: " << collection->size() << std::endl;
+    const std::vector<word_t> *profile = readProfileFromFile("profile.bin");
+    std::cout << "profile.bin: " << profile->size() << std::endl;
+    std::vector<unsigned int> *docAddresses = getDocumentAddresses(collection);
+    std::cout << "docAddresses: " << docAddresses->at(0) << std::endl;
+    executeGPUImplementation(collection, profile, docAddresses, bloomFilter);
+    delete bloomFilter;
+    delete collection;
+    delete profile;
+    delete docAddresses;
+    return 0;
+}
