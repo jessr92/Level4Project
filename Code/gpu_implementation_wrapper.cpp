e7dbf15c6fbee7fbc19c00e3a920f190f2208b77
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2013-11-04 19:58:07 +0000

    Adds skeleton for OclWrapper version of host code

---
+++ Code/gpu_implementation_wrapper.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "read_files.h"
+#include "shared_details.h"
+#include "OclWrapper.h"
+
+#define THRESHOLD 0
+
+void executeGPUImplementation(const std::vector<word_t> *collection,
+                              const std::vector<word_t> *profile,
+                              std::vector<unsigned int> *docAddresses,
+                              const std::vector<word_t> *bloomFilter)
+{
+    unsigned long hamCount = 0;
+    unsigned long spamCount = 0;
+    unsigned long *scores = new unsigned long[docAddresses->at(0)];
+    for (word_t i = 0; i < docAddresses->at(0); i++)
+    {
+        scores[i] = 0;
+    }
+    // No idea why I need this but it'll crash otherwise.
+    word_t *tempProfile = new word_t[profile->size()];
+    std::copy(profile->begin(), profile->end(), tempProfile);
+    // Insert OcLWrapper stuff here
+    for (word_t i = 0; i < docAddresses->at(0); ++i)
+    {
+        scores[i] > THRESHOLD ? ++spamCount : ++hamCount;
+        std::cout << (collection->at(docAddresses->at(i + 1)) >> 4) << ":" << scores[i] << std::endl;
+    }
+    std::cout << "Ham documents: " << hamCount << std::endl;
+    std::cout << "Spam documents " << spamCount << std::endl;
+    delete [] scores;
+    delete [] tempProfile;
+}
+
+int main()
+{
+    const std::vector<word_t> *bloomFilter = loadParsedCollectionFromFile(BLOOM_FILTER_FILE);
+    std::cout << BLOOM_FILTER_FILE << ": " << bloomFilter->size() << std::endl;
+    const std::vector<word_t> *collection = loadParsedCollectionFromFile(COLLECTION_FILE);
+    std::cout << COLLECTION_FILE << ": " << collection->size() << std::endl;
+    const std::vector<word_t> *profile = readProfileFromFile(PROFILE_FILE);
+    std::cout << PROFILE_FILE << ": " << profile->size() << std::endl;
+    std::vector<unsigned int> *docAddresses = getDocumentAddresses(collection);
+    std::cout << "docAddresses: " << docAddresses->at(0) << std::endl;
+    executeGPUImplementation(collection, profile, docAddresses, bloomFilter);
+    delete bloomFilter;
+    delete collection;
+    delete profile;
+    delete docAddresses;
+    return 0;
+}
