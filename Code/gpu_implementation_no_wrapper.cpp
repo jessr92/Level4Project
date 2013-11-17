886ab28783f62c70c057f0abd5e151a8a76f66e4
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2013-11-17 21:29:48 +0000

    Fixes bloom filter
    OpenCL doesn't like std::vector

--- Code/gpu_implementation_no_wrapper.cpp
+++ Code/gpu_implementation_no_wrapper.cpp
@@ -49,6 +49,8 @@ void executeGPUImplementation(const std::vector<word_t> *collection,
     std::copy(docAddresses->begin(), docAddresses->end(), tempDocAddresses);
     word_t *tempProfile = new word_t[profile->size()];
     std::copy(profile->begin(), profile->end(), tempProfile);
+    word_t *tempBloomFilter = new word_t[bloomFilter->size()];
+    std::copy(bloomFilter->begin(), bloomFilter->end(), tempBloomFilter);
     try
     {
         mark_time();
@@ -82,7 +84,7 @@ void executeGPUImplementation(const std::vector<word_t> *collection,
         queue.enqueueWriteBuffer(d_docAddresses, CL_TRUE, 0, docAddressesSize, tempDocAddresses);
         queue.enqueueWriteBuffer(d_scores, CL_TRUE, 0, scoresSize, scores);
 #ifdef BLOOM_FILTER
-        queue.enqueueWriteBuffer(d_bloomFilter, CL_TRUE, 0, bloomFilterSize, bloomFilter);
+        queue.enqueueWriteBuffer(d_bloomFilter, CL_TRUE, 0, bloomFilterSize, tempBloomFilter);
 #endif
         // Read the program source
         std::ifstream sourceFile(KERNEL_FILE);
