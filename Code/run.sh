979037aa49bf337cf4c6c1b7f8aac3a9e10f7ca4
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2013-11-17 19:31:58 +0000

    Add descriptive output + sanity check to run.sh
    the non-optimised cpu reference output is diff'ed with new
    cpu_reference_implementation.

--- Code/run.sh
+++ Code/run.sh
@@ -1,7 +1,12 @@
 make
-./cpu_reference_implementation > cpuRef.txt
+echo "Executing CPU Reference Implementation"
+./cpu_reference_implementation > cpuRefNoOptimise.txt
+echo "Executing CPU OpenCL Implementation"
 ./cpu_implementation_no_wrapper > cpu.txt
+echo "Executing GPU OpenCL Implementation"
 ./gpu_implementation_no_wrapper > gpu.txt
+echo "---------cpu.txt versus cpuRefNoOptimise.txt---------"
+diff cpu.txt cpuRefNoOptimise.txt
 echo "---------cpu.txt versus gpu.txt---------"
 diff cpu.txt gpu.txt
 echo "---------cpu.txt versus cpuRef.txt---------"
