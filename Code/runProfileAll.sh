a3d173f6af3a4d5749b88a4e8ac77baa4ba83cda
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2013-11-22 19:27:26 +0000

    Further refactor and bugfixing of runProfileAll.sh

--- Code/runProfileAll.sh
+++ Code/runProfileAll.sh
@@ -2,41 +2,38 @@ make clean
 make
 mkdir -p experimentRun
 echo "Starting experiment..."
-cpuRef=("cpuRefEP.txt" "cpuRefEF.txt" "cpuRefEI.txt" "cpuRefEW.txt" \
-         "cpuRefSP.txt" "cpuRefSF.txt" "cpuRefSI.txt" "cpuRefSW.txt" \
-         "cpuRefUP.txt" "cpuRefUF.txt" "cpuRefUI.txt" "cpuRefUI.txt")
-cpuRefMT=("cpuRefMTEP.txt" "cpuRefMTEF.txt" "cpuRefMTEI.txt" "cpuRefMTEW.txt" \
-         "cpuRefMTSP.txt" "cpuRefMTSF.txt" "cpuRefMTSI.txt" "cpuRefMTSW.txt" \
-         "cpuRefMTUP.txt" "cpuRefMTUF.txt" "cpuRefMTUI.txt" "cpuRefMTUI.txt")
-cpu=("cpuEP.txt" "cpuEF.txt" "cpuEI.txt" "cpuEW.txt" \
-         "cpuSP.txt" "cpuSF.txt" "cpuSI.txt" "cpuSW.txt" \
-         "cpuUP.txt" "cpuUF.txt" "cpuUI.txt" "cpuUI.txt")
-gpu=("gpuEP.txt" "gpuEF.txt" "gpuEI.txt" "gpuEW.txt" \
-         "gpuSP.txt" "gpuSF.txt" "gpuSI.txt" "gpuSW.txt" \
-         "gpuUP.txt" "gpuUF.txt" "gpuUI.txt" "gpuUI.txt")
-profiles=(profile*.bin)
+cpuRef=("cpuRefEF.txt" "cpuRefEI.txt" "cpuRefEP.txt" "cpuRefEW.txt" \
+        "cpuRefSF.txt" "cpuRefSI.txt" "cpuRefSP.txt" "cpuRefSW.txt" \
+        "cpuRefUF.txt" "cpuRefUI.txt" "cpuRefUP.txt" "cpuRefUW.txt")
+cpuRefMT=("cpuRefMTEF.txt" "cpuRefMTEI.txt" "cpuRefMTEP.txt" "cpuRefMTEW.txt" \
+          "cpuRefMTSF.txt" "cpuRefMTSI.txt" "cpuRefMTSP.txt" "cpuRefMTSW.txt" \
+          "cpuRefMTUF.txt" "cpuRefMTUI.txt" "cpuRefMTUP.txt" "cpuRefMTUW.txt")
+cpu=("cpuMTEF.txt" "cpuMTEI.txt" "cpuMTEP.txt" "cpuMTEW.txt" \
+     "cpuMTSF.txt" "cpuMTSI.txt" "cpuMTSP.txt" "cpuMTSW.txt" \
+     "cpuMTUF.txt" "cpuMTUI.txt" "cpuMTUP.txt" "cpuMTUW.txt")
+gpu=("gpuMTEF.txt" "gpuMTEI.txt" "gpuMTEP.txt" "gpuMTEW.txt" \
+     "gpuMTSF.txt" "gpuMTSI.txt" "gpuMTSP.txt" "gpuMTSW.txt" \
+     "gpuMTUF.txt" "gpuMTUI.txt" "gpuMTUP.txt" "gpuMTUW.txt")
+profiles=(profile?*.bin)
 echo "Sleeping..."
-sleep 60
+#sleep 60
 for i in "${!profiles[@]}"
 do
-    if [ "${profiles[$i]}" != "profile.bin" ];
-    then
         echo "./cpu_reference_implementation ${profiles[$i]} > experimentRun/${cpuRef[$i]}"
         ./cpu_reference_implementation "${profiles[$i]}" > "experimentRun/${cpuRef[$i]}"
         echo "Sleeping..."
         sleep 60
-        echo "./cpu_reference_implementation_mt ${profiles[$i]} > experimentRun/${cpuRef[$i]}"
+        echo "./cpu_reference_implementation_mt ${profiles[$i]} > experimentRun/${cpuRefMT[$i]}"
         ./cpu_reference_implementation_mt "${profiles[$i]}" > "experimentRun/${cpuRefMT[$i]}"
         echo "Sleeping..."
         sleep 60
-        echo "./cpu_implementation_no_wrapper ${profiles[$i]} > experimentRun/${cpuRef[$i]}"
+        echo "./cpu_implementation_no_wrapper ${profiles[$i]} > experimentRun/${cpu[$i]}"
         ./cpu_implementation_no_wrapper "${profiles[$i]}" > "experimentRun/${cpu[$i]}"
         echo "Sleeping..."
         sleep 60
-        echo "./gpu_implementation_no_wrapper ${profiles[$i]} > experimentRun/${cpuRef[$i]}"
+        echo "./gpu_implementation_no_wrapper ${profiles[$i]} > experimentRun/${gpu[$i]}"
         ./gpu_implementation_no_wrapper "${profiles[$i]}" > "experimentRun/${gpu[$i]}"
         echo "Sleeping..."
         sleep 60
-    fi
 done
 echo "Finished experiment..."
