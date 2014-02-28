2e50717b1ed18fe7a1649e03de52f616e0d50af1
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2014-02-28 13:42:49 +0000

    Adds CPU/Phi fork option to run script

--- Code/runProfileParser.sh
+++ Code/runProfileParser.sh
@@ -65,5 +65,21 @@ if [ "$HOSTNAME" = "manipa" ]; then
     ./opencl_full > parserResults/Phi/opencl_fullUF.txt profileUSAFinancial.bin
     ./opencl_full > parserResults/Phi/opencl_fullUI.txt profileUSAInternational.bin
     ./opencl_full > parserResults/Phi/opencl_fullUW.txt profileUSAWashington.bin
+    make clean
+    make
+    mkdir -p parserResults/CPUPhi
+    echo "Testing parser/scorer on CPU and GPU"
+    ./opencl_full_fork > parserResults/CPUPhi/opencl_full_forkEP.txt profileEntertainmentPolitical.bin
+    ./opencl_full_fork > parserResults/CPUPhi/opencl_full_forkEF.txt profileEntertainmentFinancial.bin
+    ./opencl_full_fork > parserResults/CPUPhi/opencl_full_forkEI.txt profileEntertainmentInternational.bin
+    ./opencl_full_fork > parserResults/CPUPhi/opencl_full_forkEW.txt profileEntertainmentWashington.bin
+    ./opencl_full_fork > parserResults/CPUPhi/opencl_full_forkSP.txt profileSportsPolitical.bin
+    ./opencl_full_fork > parserResults/CPUPhi/opencl_full_forkSF.txt profileSportsFinancial.bin
+    ./opencl_full_fork > parserResults/CPUPhi/opencl_full_forkSI.txt profileSportsInternational.bin
+    ./opencl_full_fork > parserResults/CPUPhi/opencl_full_forkSW.txt profileSportsWashington.bin
+    ./opencl_full_fork > parserResults/CPUPhi/opencl_full_forkUP.txt profileUSAPolitical.bin
+    ./opencl_full_fork > parserResults/CPUPhi/opencl_full_forkUF.txt profileUSAFinancial.bin
+    ./opencl_full_fork > parserResults/CPUPhi/opencl_full_forkUI.txt profileUSAInternational.bin
+    ./opencl_full_fork > parserResults/CPUPhi/opencl_full_forkUW.txt profileUSAWashington.bin
 fi
 python summary.py parserResults
